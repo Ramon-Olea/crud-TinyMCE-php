a1b3301b4a6b878f7397a789e8727685b4b01aac
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-07-26 09:50:36 -0500

    primera subida
    tiny editor

---
+++ formulario_registrar.php
@@ -0,0 +1,40 @@
+<?php
+  ?>
+<?php include_once "encabezado.php"; ?>
+<script src="https://cdn.tiny.cloud/1/wc7826d94l1swoitws17wlq02h8thlk5orj0fj9ikbswnekr/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script> /* poner el codigo de tu cuenta */
+        
+tinymce.init({
+  selector: 'textarea#descripcion',
+  height: 500,
+  menubar: false,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table paste code help wordcount'
+  ],
+  toolbar: 'undo redo | formatselect | ' +
+  'bold italic backcolor | alignleft aligncenter ' +
+  'alignright alignjustify | bullist numlist outdent indent | ' +
+  'removeformat | help',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+});
+
+    </script>
+<div class="row">
+    <div class="col-12">
+        <h1>Registrar videojuego</h1>
+        <form action="registrar.php" method="POST">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input placeholder="Nombre" class="form-control" type="text" name="nombre" id="nombre" required>
+            </div>
+            <div class="form-group">
+                <label for="descripcion">Descripción</label>
+                <textarea placeholder="Descripción" class="form-control" name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
+            </div>
+            <div class="form-group"><button class="btn btn-success">Guardar</button></div>
+        </form>
+    </div>
+</div>
+<?php include_once "pie.php"; ?>
